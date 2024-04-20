d8eddc8e4944499979670f0c74eb335efc048d5f
hatif <hb8784@gmail.com>
2024-04-20 06:47:19 +0530

    added select image

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import AppLayout from "../component/layout/AppLayout";
 import { Box, Typography } from "@mui/material";
+import selectChatting from "../assets/select-chat/chatting.svg";
 
 const Home = () => {
   return (
     <Box bgcolor={"#cbd5e1"} height={"100%"}>
       <Typography textAlign={"center"} variant="h5" p={"2rem"}>
-        Select a chat to continue
+        <pre style={{ textWrap: "pretty" }}>
+          click on <b>&#9776;</b> icon to select chat on Mobile
+        </pre>
+        <img
+          src={selectChatting}
+          style={{ marginTop: "20rem", width: "100%" }}
+        />
       </Typography>
     </Box>
   );
